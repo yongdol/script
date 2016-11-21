39ac633a40fa4a782a36401dc4ebdaf2a933e43b
yongdol <kungbuk77@gmail.com>
2016-11-21 15:56:26 +0900

    수정

--- make_django_project.sh
+++ make_django_project.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #폴더를 생성할 위치에서 실행
-# sh make_django_project.sh <프로젝트이름> <pyenv이름>
+# sh <다운받은 make_django_project.sh 의 dir> <프로젝트이름> <pyenv이름>
 project_dir=`pwd`
 project_name=$1
 pyenv_name=$2
@@ -13,6 +13,9 @@ pip install django
 pip freeze > requirements.txt
 django-admin startproject $project_name
 mv $project_name django_app
+
+#복사할 gitignore의 위치를 자신의 환경에 맞게 변경해서 사용.
 cp ~/project/django/instagram/.gitignore .
-#pycharm 실행
+
+#pycharm 실행(macOS)
 open -a /Applications/PyCharm\ CE.app/Contents/MacOS/pycharm $project_dir/$project_name
